c1c3a831c50c788c18e5163ac9cc878867fe6c2a
aswitha <aswithaavijay@gmail.com>
2026-06-11 21:40:15 +0530

    git

---
+++ JS/arrayMethod.js
@@ -0,0 +1,24 @@
+
+// const employees = [ { id: 1, name: "John", salary: 25000 }, 
+//     { id: 2, name: "David", salary: 45000 }, { id: 3, name: "Sam", salary: 60000 },
+//      { id: 4, name: "Peter", salary: 30000 } ]; 
+
+
+//     const fitereddata= employees.filter((f)=>f.salary>4000)
+
+// fitereddata.forEach((e)=>{
+//     console.log(e.name,e.salary);
+    
+// })
+
+const users = 
+[ { id: 1, name: "Sudhan" },
+     { id: 2, name: "Kumar" }, 
+     { id: 3, name: "Ravi" } ];
+
+     const findUsers=users.find((f)=>f.id===2)
+
+    console.log(findUsers.name,findUsers.id);
+    
+
+     
