9bae0f27eb8e02830597f634a391a9cc2a9cda7a
aswitha <aswithaavijay@gmail.com>
2026-06-12 19:05:58 +0530

    task 5

--- JS/arrayMethod.js
+++ JS/arrayMethod.js
@@ -21,11 +21,42 @@
 //     console.log(findUsers.name,findUsers.id);
     
 
-      const cart = [ { product: "Laptop", price: 50000 }, { product: "Mouse", price: 500 }, { product: "Keyboard", price: 1500 } ]; 
+//       const cart = [ { product: "Laptop", price: 50000 }, { product: "Mouse", price: 500 }, { product: "Keyboard", price: 1500 } ]; 
 
-     const priceTotal= cart.reduce((acc,curvalue)=>{
-                return acc+curvalue.price
-          },0)
+//      const priceTotal= cart.reduce((acc,curvalue)=>{
+//                 return acc+curvalue.price
+//           },0)
 
-          console.log(priceTotal);
-          
+//           console.log(priceTotal);
+
+
+           const students = [ { id: 1, name: "Arun", mark: 85 }, 
+            { id: 2, name: "Karthik", mark: 45 }, 
+            { id: 3, name: "Vijay", mark: 92 },
+             { id: 4, name: "Ajay", mark: 35 } ]
+
+
+            const data=students.map((e)=> {return{
+                  name: e.name,
+                  id:e.id}
+            })
+            
+            console.log(data);
+            
+
+            const filterData=students.filter((f)=>f.mark>=50)
+
+            console.log(filterData);
+            
+            const findID=students.find((f)=>f.id==2)
+            console.log(findID);
+            
+
+            const totalMark=students.reduce((acc,curvalue)=>{return acc+curvalue.mark},0)
+
+            console.log(totalMark);
+            
+            const avgMark=students.reduce((acc,curvalue)=>{return (acc+curvalue.mark)},0)/students.length
+
+            console.log(avgMark);
+            
